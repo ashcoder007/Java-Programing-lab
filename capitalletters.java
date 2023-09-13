c3ee5b7152111965710201200fdb3fc9246594ad
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-09-13 09:07:46 +0530

    Add files via upload

---
+++ capitalletters.java
@@ -0,0 +1,11 @@
+
+public class capitalletters
+{
+    public static void main(String[] args)
+    {
+        for (char i='A';i<='Z';i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
