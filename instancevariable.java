610bff5667813c3053622cee568108463ad30d69
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-09-13 09:17:41 +0530

    Add files via upload

---
+++ instancevariable.java
@@ -0,0 +1,7 @@
+public class instancevariable{
+    int x=5;
+    public static void main(String[] args) {
+       instancevariable obj=new instancevariable();
+        System.out.print("The value assigned to instance variable is "+obj.x);
+    }
+}
