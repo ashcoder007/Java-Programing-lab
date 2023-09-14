4c04ad8e613ac38e525299138d8cf417b773df9b
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-09-14 20:51:59 +0530

    Add files via upload

---
+++ checkforimmutablity.java
@@ -0,0 +1,12 @@
+class checkforimmutablity {
+    public static void main(String[] args)
+    {
+        String first="JAVA";
+        String second="PYTHON";
+        String third="javascript";
+        String first="hello";
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+    }
+}
