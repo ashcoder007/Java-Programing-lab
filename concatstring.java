6a248dc7b4bea9ef4e2dc373df8f49f21bdd2e36
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-09-14 20:53:06 +0530

    Add files via upload

---
+++ concatstring.java
@@ -0,0 +1,14 @@
+
+public class concatstring
+{
+	public static void main(String[] args) {
+	    String first="java";
+	    System.out.println("First String:"+first);
+	    
+	    String second="Programming";
+	    System.out.println("Second String:"+second);
+	    
+	    String joinedString = first.concat(second);
+	     System.out.println("Joined String:"+ joinedString);
+	}
+}
