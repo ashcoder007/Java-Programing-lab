a14cdc3df0e7723994e4da1033db75d5026c332e
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-09-26 12:17:25 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,13 @@
+//to demonstrate for each loop using arrays 
+public class array2
+{
+	public static void main(String[] args)
+	{
+	     int[]age =new int[]{1,2,3};
+	      System.out.println(age.length);
+	      for(int a:age)
+	      {
+	           System.out.println(a);
+	      }
+	}
+}
