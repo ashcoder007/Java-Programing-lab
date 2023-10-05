132bdf42267bbbbdf133367397463b3c6ccf1d9c
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-10-05 12:54:11 +0530

    Add files via upload

---
+++ Twodimensionalarray4.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Twodimensionalarray4
+{
+	public static void main(String[] args) {
+		String[][] salutation= {{"Mr.","Mrs","Ms"},{"Kumar"}};
+		System.out.println(salutation[0][0] + " "+salutation[1][0]);
+		System.out.println(salutation[0][1] + salutation[1][0]);
+}
+}
