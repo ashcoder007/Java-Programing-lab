6b2e96d344828e12f90dc2e6aca37d51cd6b6807
ashcoder007 <143689066+ashcoder007@users.noreply.github.com>
2023-10-17 12:16:25 +0530

    Add files via upload

---
+++ errorhandling.java
@@ -0,0 +1,13 @@
+public class errorhandling{
+    public static void main(String[] args){
+    try{
+        int x=100;
+        int y=0;
+        int z=x/y;
+        System.out.println(z);
+    }
+    catch (Exception err){
+        System.out.println("my nAME IS ASHISH");
+    }
+    }
+}
